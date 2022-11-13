df2fbbafd02ee05600ce4f937cbb4c6a2fc60c5e
Harp <walesii19@gmail.com>
2022-11-13 15:54:58 +0000

    second commit

---
+++ shell_main.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * main - method to read input form user
+ *
+ * Return: 0 on success
+ *
+ */
+
+int main(void)
+{
+	int stat;
+
+	do
+	{
+		stat = promptShell();
+	} while (stat != _SHELL_END);
+
+	return (EXIT_SUCCESS);
+}
+
+
